e262e42de1a7d91af2fcd805687273f515cbab3e
Rin <akamine.423909@occ.co.jp>
2022-05-09 16:55:36 +0900

    dev-add

---
+++ ConsoleApp2/ConsoleApp2/Classmainas1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Classmainas1
+    {
+        class Add
+        {
+
+            public int add(int a, int b)
+            {
+                return a - b;
+            }
+        }
+    }
+}
