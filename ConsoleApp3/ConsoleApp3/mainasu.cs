9a593516a4802e0525dc404ede4cdb89172ccdf8
Rin <akamine.423909@occ.co.jp>
2022-05-09 17:18:39 +0900

    kakikukekomainasudev-add

---
+++ ConsoleApp3/ConsoleApp3/mainasu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class mainasu
+    {
+        class Add
+        {
+
+            public int add(int a, int b)
+            {
+                return a - b;
+            }
+        }
+    }
+}
